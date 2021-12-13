99ae2cee9c223d9e9696a742b98ac5fe1056cb42
jhrs <66736876+JansonSiy@users.noreply.github.com>
2021-12-13 18:09:25 +0800

    fixed comments and renamed variables

--- eCommerceApp/projectFolder_eCommerceApp/accounts/views.py
+++ eCommerceApp/projectFolder_eCommerceApp/accounts/views.py
@@ -8,17 +8,17 @@ def signup(request):
     # if it's a post request
     if request.method =='POST':
         # passing all the values/data of the post request to a new instance
-        form = UserCreationForm(request.POST)
+        postReqForm = UserCreationForm(request.POST)
         # if the form is valid (follows the instructions imbeded in django form) it saves the data
         # .is_valid() is a built in method that can check that, that returns boolean value
-        if form.is_valid():
-            form.save()
+        if postReqForm.is_valid():
+            postReqForm.save()
             # then redirect('appNameInUrl.py:urlName')
             return redirect('store:mystore')
     else:
-        form = UserCreationForm()
+        newInstanceForm = UserCreationForm()
         # creating a new instance (then send it over to the template)
     
-    context = {'form': form}
+    context = {'form': newInstanceForm}
     
     return render(request, 'accounts/signup.html', context)
